ccca37c0a733ceee0642873311b2ac11824a2654
Bartomiej Król <krolbartlomiej1122@gmail.com>
2023-11-04 15:51:31 +0100

    dodanie elementu z kontaktem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Slider from '../components/Slider/Slider';
+import Kontakt from "../components/Kontakt/Kontakt";
 
 const Home = () => {
     return (
@@ -8,8 +9,7 @@ const Home = () => {
                 <Slider />
             </div>
             <div className="kontakt">
-                POTRZEBUJESZ KARNET NA DŁUŻSZY CZAS?
-                Skontaktuj się z nami!
+                <Kontakt/>
             </div>
             <div className="porady">
                 porady?
