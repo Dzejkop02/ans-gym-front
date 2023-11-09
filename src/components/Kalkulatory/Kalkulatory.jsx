58f2b57c344479a5abebbaa551a7779873bc1037
MikolajNiewola <mikolaj.niewola.321@gmail.com>
2023-11-09 16:31:53 +0100

    main-section-update
    poprawa styli css

--- src/components/Kalkulatory/Kalkulatory.jsx
+++ src/components/Kalkulatory/Kalkulatory.jsx
@@ -7,10 +7,10 @@ const Kalkulatory = () => {
             <div className="kleft">
                 <h1 className="L-word">LET'S</h1>
                 <div className="k-images">
-                    <img src="/zdj/measuring-tape.png" alt="kalkulator-1"></img>
-                    <img src="/zdj/gym.png" alt="kalkulator-2"></img>
-                    <img src="/zdj/drink.png" alt="kalkulator-3"></img>
-                    <img src="/zdj/body.png" alt="kalkulator-4"></img>
+                    <img src="/zdj/measuring-tape.png" alt="kalkulator-1" className=".k-img"></img>
+                    <img src="/zdj/gym.png" alt="kalkulator-2" className=".k-img"></img>
+                    <img src="/zdj/drink.png" alt="kalkulator-3" className=".k-img"></img>
+                    <img src="/zdj/body.png" alt="kalkulator-4" className=".k-img"></img>
                 </div>
             </div>
             <div className="kright">
