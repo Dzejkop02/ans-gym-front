66743cc48f655fcc0fa21911b3ecdd83a2b00d84
Bartomiej Król <krolbartlomiej1122@gmail.com>
2023-11-30 15:20:31 +0100

    utworzenie podstrony galeria

--- src/pages/galeria.jsx
+++ src/pages/galeria.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Slider from '../components/Slider/Slider';
+
+import Footer from '../components/Footer/Footer';
+
+import Gallery from "../components/Galeria/galeria";
 
 const Galeria = () => {
     return (
         <div>
-            Galeria
+           <div className="slider-box">
+                <Slider />
+            </div>
+            <div className="stopka">
+                <Gallery />
+            </div>
+            <div className="stopka">
+                <Footer />
+            </div>
         </div>
 
     );
