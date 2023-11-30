0930a6144f1d1b944956bf6b447ca7d8cd70edc9
Lebron02 <szymongrzegorzadamczyk@gmail.com>
2023-11-30 17:54:32 +0100

    dodano zawartosc strony O nas

---
+++ src/components/Atuty/Atuty.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './Atuty.css';
+
+const Atuty = () => {
+    return(
+        <div className="atuty">
+            <div className="divonasimg">
+                <img className="onasimg" src="zdj/victor-freitas-nlZTjUZX2qo-unsplash.jpg" alt="vf"/>
+            </div>
+            <div className="acontent">
+                
+                <div className="doswiadczenie">
+                    <img className="doswiadczenie" src="zdj/calendar-2.png" alt="cl"/>
+                    <h1>Doświadczenie</h1>
+                    <p>Istniejemy od 2012 roku. Inwestujemy w ciągły rozwój naszej marki i wzmacnianie jej pozycji na rynku w branży fitness</p>
+                </div>
+                <div className="ekspansja">
+                    <img className="ekspansja" src="zdj/chart.png" alt="ch"/>
+                    <h1 className="h1ekspansja">Ekspansja</h1>
+                    <p>Istniejemy od 2012 roku. Inwestujemy w ciągły rozwój naszej marki i wzmacnianie jej pozycji na rynku w branży fitness</p>
+                </div>
+                <div className="definicja">
+                    <img className="definicja" src="zdj/deal.png" alt="dl"/>
+                    <h1>Definicja biznesowa</h1>
+                    <p>Tworzymy miejsca aktywnego spędzania czasu, budujące więzi społeczne w oparciu o kulturę sportową, by umożliwić rozwój ciała i wiele innych</p>
+                </div>
+                <div className="nowoczesnosc">
+                    <img className="nowoczesnosc" src="zdj/technology.png" alt="th"/>
+                    <h1>Nowoczesność</h1>
+                    <p>Posiadamy autorskie narzędzia dedykowane naszym klubowiczom</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Atuty;
--- src/pages/onas.jsx
+++ src/pages/onas.jsx
@@ -1,9 +1,29 @@
 import React from "react";
+import Slider from '../components/Slider/Slider';
+import Kontakt from "../components/Kontakt/Kontakt";
+import Footer from '../components/Footer/Footer';
+import Map from '../components/Map/Map';
+import Atuty from "../components/Atuty/Atuty";
+import Onasopis from "../components/Onas-opis/onas-opis";
 
 const Onas = () => {
     return (
         <div>
-            O NAS
+            <div className="atuty">
+                <Atuty />
+            </div>
+            <div className="onas-opis">
+                <Onasopis />
+            </div>
+            <div className="kontakt">
+                <Kontakt/>
+            </div>
+            <div className="mapa">
+                <Map />
+            </div>
+            <div className="stopka">
+                <Footer />
+            </div>
         </div>
 
     );
