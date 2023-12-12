a48ab5aa0af770db5f9b412c842e4e3495a90985
MikolajNiewola <mikolaj.niewola.321@gmail.com>
2023-12-12 12:13:45 +0100

    poprawa styli na responsywne

--- src/components/Kontakt/Kontakt.jsx
+++ src/components/Kontakt/Kontakt.jsx
@@ -2,20 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './Kontakt.css';
 
+
 const Kontakt = () => {
     return(
       <div className="kontakt">
-        <div className="lewo">
-            <h2>POTRZEBUJESZ KARNET NA DŁUŻSZY CZAS     ?</h2>
-            <h3>Skontaktuj się z nami!</h3>
+        <div className="kontakt-calosc">
+          <div className="kontakt-lewo">
+              <h2>POTRZEBUJESZ KARNET NA DŁUŻSZY CZAS     ?</h2>
+              <p>Skontaktuj się z nami!</p>
+          </div>
+          <div className="kontakt-prawo">
+            <Link to="/kontakt">
+              NAPISZ DO NAS
+            </Link>
+          </div>
         </div>
-        <Link to="/kontakt">
-        <div className="prawo">
-            <h2>NAPISZ DO NAS</h2>
-        </div>
-        </Link>
-        
       </div>
+      
     );
 };
 
