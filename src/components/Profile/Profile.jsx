977100a1b4021377800afbbeea205063ac42eb16
Lebron02 <szymongrzegorzadamczyk@gmail.com>
2024-01-18 14:54:00 +0100

    dodanie funkcjonalnosci wylogowania oraz wyswietlenia zakupionego karnetu

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import './Profile.css';
 
+
 const Profile = () => {
+    const [userData, setUserData] = useState({
+        email: '',
+        firstName: '',
+        lastName: '',
+        loggedIn: false,
+        ticketName: null,
+        ticketUntil: null,
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:3001/auth', {
+                    credentials: 'include',
+                });
+
+                const data = await response.json();
+
+                if (data.loggedIn) {
+                    // Jeżeli użytkownik jest zalogowany, aktualizuj stan komponentu
+                    setUserData(data);
+                } else {
+                    // W przeciwnym razie, przekieruj użytkownika na stronę logowania
+                    window.location.href = '/login'; // Załóżmy, że masz stronę logowania na "/login"
+                }
+            } catch (error) {
+                console.error('Wystąpił błąd podczas pobierania danych użytkownika:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleLogout = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/auth/logout', {
+                credentials: 'include',
+            });
+
+            const data = await response.json();
+
+            if (data.success) {
+                // Po udanym wylogowaniu przekieruj użytkownika na stronę główną
+                window.location.href = '/';
+            } else {
+                console.error('Wystąpił błąd podczas wylogowywania:', data.error || 'Nieznany błąd.');
+            }
+        } catch (error) {
+            console.error('Wystąpił błąd podczas wylogowywania:', error);
+        }
+    };
+
+    const formatTicketUntil = () => {
+        if (userData.ticketUntil) {
+            const date = new Date(userData.ticketUntil);
+            const formattedDate = date.toLocaleDateString('pl-PL');
+            return formattedDate;
+        } else {
+            return 'Brak informacji';
+        }
+    };
+
     return(
-        <div className="map">
-           <div className="mleft">
-                <h1>LOREM IPSUM IS SIMPLY</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type sp</p>
-           </div>
-            <div className="mright">
-              <iframe
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  style={{ border: 0 }}
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2633.9046334296366!2d20.986074315738266!3d50.013263579419615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4715f61ea23cd227%3A0xd3c04e4902a3dfc1!2sMickiewicza%208%2C%2033-100%20Tarn%C3%B3w%2C%20Poland!5e0!3m2!1sen!2sus!4v1644708407406!5m2!1sen!2sus"
-                  allowFullScreen
-              ></iframe>
+        <div className="profil">
+            <div className="profil-background">
+            <div className="profil-left">
+                <div className="profil-up">
+                    <img className="user-img" src="zdj/user.png" alt="user"/>
+
+                    <p>{userData.firstName + ' ' + userData.lastName}</p>
+                    <p className="grey">{userData.email}</p>
+                </div>
+                <div className="profil-down">
+                    <p>Wykupiony karnet: </p>
+                    <p className="red">{userData.ticketName}</p>
+                    <p>Okres ważności: </p>
+                    <p className="red">{formatTicketUntil()}</p>
+                </div>
+
+                <Link to="/" id="przycisk" className="button" onClick={handleLogout}>Wyloguj</Link>
+                
             </div>
+        </div>
         </div>
     );
 };
